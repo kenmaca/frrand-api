b0ef3317380b8f72b45755b0e1d04916c9700c63
Kenneth Ma <k.ma@utoronto.ca>
2015-09-04 23:51:36 -0400

    Added: Geocoding for Addresses ORM

--- models/addresses.py
+++ models/addresses.py
@@ -1,4 +1,5 @@
 from models.orm import MongoORM
+from geopy.geocoders import GoogleV3
 
 class Address(MongoORM):
     ''' A representation of an Address in Frrand.
@@ -14,9 +15,29 @@ class Address(MongoORM):
         MongoORM.__init__(self, db, Address.collection, **fields)
 
     @staticmethod
-    def fromObjectId(source, objectId):
-        ''' (bson.ObjectId) -> Address
-        Creates an Address directly from database with an ObjectId of objectId.
+    def fromObjectId(db, objectId):
+        ''' (pymongo.database.Database, bson.ObjectId) -> Address
+        Creates an Address directly from db with an ObjectId of objectId.
         '''
 
-        return MongoORM.fromObjectId(source, objectId, Address)
+        return MongoORM.fromObjectId(db, objectId, Address)
+
+    def geocodeAddress(self):
+        ''' (Address) -> Address
+        Fills in the address if missing from a geocoding service.
+        '''
+
+        if not self.exists('address'):
+            try:
+                self.set(
+                    'address',
+                    GoogleV3().reverse(
+                        self.get('location')['coordinates'][::-1]
+                    )[0].address
+                )
+            except Exception:
+
+                # TODO: try another geocoding service
+                address['address'] = 'Unknown'
+
+        return self
