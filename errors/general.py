e53c64ea4f10692abdb0deb82557c0d969eaa7f4
Kenneth Ma <ken@kenma.ca>
2016-02-08 01:51:10 +0000

    Changed: Outdated Version HTTP Code to use 403 instead.

--- errors/general.py
+++ errors/general.py
@@ -4,5 +4,5 @@ def abortUnsupportedVersion():
     ''' Raised when attempting to connect with an outdated client.
     '''
 
-    abort(401, 'Your client is out-of-date and unsupported by the API. '
+    abort(403, 'Your client is out-of-date and unsupported by the API. '
         + 'Please upgrade to a newer version.')
