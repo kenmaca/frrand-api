785ee68cd3b6de8d7fb6bd83887aaad3feef66c2
Kenneth Ma <ken@kenma.ca>
2016-02-11 17:22:16 -0500

    Changed: Made apiKeys private

--- routes/apiKeys.py
+++ routes/apiKeys.py
@@ -22,10 +22,10 @@ schema = {
 
 config = {
     'item_title': 'apiKey',
-    'public_methods': ['GET'],
+    'public_methods': [],
     'public_item_methods': [],
     'allowed_filters': [],
-    'resource_methods': ['POST', 'GET'],
+    'resource_methods': ['POST'],
     'schema': schema,
     'authentication': UserAuth()
 }
