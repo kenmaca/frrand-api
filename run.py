6c1572c34d958c94b1e62155163dadbee3602377
Kenneth Ma <ken@kenma.ca>
2016-04-02 14:45:49 -0400

    Added: Alexs admin-panel

--- run.py
+++ run.py
@@ -3,6 +3,8 @@
 from eve import Eve
 from eve_docs import eve_docs
 from flask_bootstrap import Bootstrap
+from flask import render_template, send_from_directory
+import os
 from utils.auth import APIAuth
 from settings import init
 from tornado.wsgi import WSGIContainer
@@ -10,9 +12,16 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 import tornado.options
 
+admin_directory = os.path.dirname(os.path.abspath(__file__)) + '/admin-panel'
+
 # start eve
-app = Eve(auth=APIAuth())
+app = Eve(auth=APIAuth(), static_folder=admin_directory)
 init(app)
+
+# admin-panel SPA
+@app.route('/admin')
+def admin_panel():
+  return send_from_directory(admin_directory, 'index.html')
 
 # eve_docs addon
 Bootstrap(app)
